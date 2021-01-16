0ba7dc23710b1a09e25aa5ce906f945c426c44b6
Max Bregman <bregmanmax91@gmail.com>
2021-01-16 15:39:55 -0700

    got promises fullfulled and now I am getting my data into my index of js

--- src/APICalls.js
+++ src/APICalls.js
@@ -1,8 +1,51 @@
-const users = fetch('http://localhost:3001/api/v1/travelers')
-    .then(response => response.json())
-    .then(data => console.log(data))
+// const users = fetch('http://localhost:3001/api/v1/getTrips() {
+// fetch('http://localhost:3001/api/v1/destinations')
+//     .then(response => response.json())
+
+// }
+// ')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
 
 
-const trips = fetch('http://localhost:3001/api/v1/trips')
-    .then(response => response.json())
-    .then(data => console.log(data))
+// const trips = fetch('http://localhost:3001/api/v1/trips')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+
+// const destinations = fetch('http://localhost:3001/api/v1/destinations')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+
+
+let apiCalls = {
+    loadData(url) {
+        return fetch(url)
+            .then(response => response.json())
+            // .then(data => data.data)
+            // .then(data => )
+    },
+    getTrips() {
+        return fetch('http://localhost:3001/api/v1/destinations')
+            .then(response => response.json())
+
+    },
+    getTravelers() {
+        return fetch('http://localhost:3001/api/v1/travelers')
+            .then(response => response.json())
+
+    },
+    getDestinations() {
+        return fetch('http://localhost:3001/api/v1/destinations')
+            .then(response => response.json())
+
+    }
+    // postData() {
+
+    // }
+    // console.log('sdfhsdofhd')
+    //     fetch('http://localhost:3001/api/v1/travelers')
+    //     .then(response => response.json())
+    //     .then(data => console.log(data))
+    // }
+}
+export default apiCalls
--- src/index.js
+++ src/index.js
@@ -3,9 +3,35 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-import './APICalls.js'
+import apiCalls from './APICalls.js';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+
+
+
+
 console.log('This is the JavaScript entry file - your code begins here.');
-window.addEventListener('load', users)
+// window.addEventListener('load', setData)
+// const fetchedTravelers = apiCalls.loadData('http://localhost:3001/api/v1/travelers')
+
+const fetchedTravelers = apiCalls.getTravelers()
+const fetchedTrips = apiCalls.getTrips()
+const fetchedDestinations = apiCalls.getDestinations()
+
+
+Promise.all([fetchedTravelers, fetchedTrips, fetchedDestinations])
+    .then(values => {
+        showStuff(values[0])
+        showStuff(values[1])
+        showStuff(values[2])
+    }).catch('o no!')
+
+
+function showStuff(stuff) {
+    console.log(stuff)
+}
+
+// function setData() {
+//     travelers = apiCalls.loadData('http://localhost:3001/api/v1/travelers')
+// }
