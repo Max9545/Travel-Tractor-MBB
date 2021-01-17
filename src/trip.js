df636b4ade204af2b080828fd3814be27e3c773b
Max Bregman <bregmanmax91@gmail.com>
2021-01-16 20:30:09 -0700

    final cleanup

--- src/trip.js
+++ src/trip.js
@@ -1,6 +1,9 @@
 class Trip {
     constructor(tripInfo) {
         this.date = tripInfo.date
+        this.duration = tripInfo.duration
+        this.travelers = tripInfo.travelers
+        this.destinationID = tripInfo.destinationID
     }
 }
 module.exports = Trip
